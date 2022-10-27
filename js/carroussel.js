a8a0b33e28bda99f55b739ec929c4d91eabd284e
Raritetnik <mkodarenko@gmail.com>
2022-10-27 15:49:30 -0400

    c12 - Intéraction avec la carroussel

--- js/carroussel.js
+++ js/carroussel.js
@@ -1,3 +1,9 @@
-/*{function() {
-    console.log('carroussel')
-}}() */
+(function(){
+    console.log("carroussel");
+    let btn = document.querySelector('.bouton');
+    btn.addEventListener('mousedown', function() {
+        console.log('Ouverture de carroussel');
+    })
+})();
+
+console.log("carroussel");
